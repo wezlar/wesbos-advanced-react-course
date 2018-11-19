bcf47fd63fcc7121b7382ed01e37886fcfc9dc1f
Dean Pugh <dean.pugh@15gifts.com>
2018-11-19 08:13:46 +0000

    Includes video 19

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     // below replaces the items function below
     items: forwardTo('db'),
+    item: forwardTo('db'),
     // async items(parent, args, ctx, info) {
     //     const items = await ctx.db.query.items();
     //     return items
