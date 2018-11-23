75715711f4c6069a05f155516f5497b28dae6f34
Dean Pugh <dean.pugh@15gifts.com>
2018-11-23 08:08:26 +0000

    Add video 24 - backend user creation code

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,40 +1,71 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO: Check if they are logged in
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
 
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ... args
-            }
-        }, info)
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    )
 
-        return item;
-    },
-    updateItem(parent, args, ctx, info) {
-        // first take a copy of the updates
-        const updates = { ...args };
-        // remove the ID from the updates
-        delete updates.id;
-        // run the updates method
-        return ctx.db.mutation.updateItem(
-            {
-                data: updates,
-                where: {
-                    id: args.id
-                }
-            },
-            info
-        );
-    },
-    async deleteItem (parent, args, ctx, info) {
-        const where = { id: args.id };
-        // 1. find the item
-        const item = await ctx.db.query.item({ where }, `{ id title }`);
-        // 2. check if they own that item or have the permission
-        // TODO
-        // 3. delete it
-        return ctx.db.mutation.deleteItem({ where }, info);
-    },
-};
+    return item
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args }
+    // remove the ID from the updates
+    delete updates.id
+    // run the updates method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    )
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    // 2. check if they own that item or have the permission
+    // TODO
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase()
+    // hash the password
+    const password = await bcrypt.hash(args.password, 10)
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        }
+      },
+      info
+    );
+    // create the jwt token for them
+    const token = jwt.sign({ user: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // finally we return the user to the browser
+    return user
+  }
+}
 
-module.exports = Mutations;
+module.exports = Mutations
