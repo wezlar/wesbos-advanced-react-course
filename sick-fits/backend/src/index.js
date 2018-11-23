7a4f6e1f07c0f5520ee9a6769480b5d86014168c
Dean Pugh <dean.pugh@15gifts.com>
2018-11-23 16:38:34 +0000

    Up to video 26 adding user jwt usage

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,25 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
+
+// cookie parser middleware
 server.express.use(cookieParser());
-// TODO use express middleware to populate current user
+
+// middleware decode the jwt so we can get the user id on each request
+server.express.use((req, res, next) => {
+    const { token } = req.cookies;
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        // put the userid onto the req for future requests to access
+        req.userId = userId;
+    }
+    next();
+});
 
 server.start({
     cors: {
