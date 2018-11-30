dea0a6503d210b7fca3351aec1596ec0457162c5
Dean Pugh <dean.pugh@15gifts.com>
2018-11-30 21:31:49 +0000

    Video 30 - Reset password front end. Needs testing

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -108,7 +108,6 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpiry },
     });
-    console.log(res);
     return { message: '"Thanks!' };
     // email them that reset token
   },
