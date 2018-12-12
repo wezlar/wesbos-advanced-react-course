2a37672f382f31b074c1a0020becc3efe9234b05
Dean Pugh <dean.pugh@15gifts.com>
2018-12-12 08:03:09 +0000

    Video 40 - Cart backend functionality and addtocart component creation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -199,6 +199,40 @@ const Mutations = {
         id: args.userId
       }
     }, info);
+  },
+  async addToCart(parent, args, ctx, info) {
+    // 1. make sure they are signed in
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('You must be signed in'); 
+    }
+    // 2. query the users current cart
+    const [existingCartItem] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId},
+        item: { id: args.id },
+      }
+    });
+    // 3. check if that item is already in their cart and increment by one if it is
+    if (existingCartItem) {
+      console.log('This item is already in their cart');
+      return ctx.db.mutation.updateCartItem({
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + 1 }
+      });
+    }
+    // 4. if its not, create a fresh cart item for that user
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: {
+          connect: { id: userId },
+        },
+        item: {
+          connect: { id: args.id },
+        },
+      }
+    });
+
   }
 }
 
