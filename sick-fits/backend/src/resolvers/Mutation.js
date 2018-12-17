de99527b338cabecb8eed8559c4ff342b93bf1ad
Dean Pugh <dean.pugh@15gifts.com>
2018-12-17 07:33:24 +0000

    Video 42

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -232,8 +232,25 @@ const Mutations = {
         },
       }
     });
-
-  }
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. find the cart itmem
+    const cartItem = await ctx.db.query.cartItem({
+      where: {
+        id: args.id,
+      },
+    }, `{ id, user { id }}`);
+    // 1.5 make sure we found an item
+    if (!cartItem) throw new Error('No CartItem Found!');
+    // 2. make sure they own the cart
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheatin huhhhhh');
+    }
+    // 3. delete that cart item
+    return ctx.db.mutation.deleteCartItem({
+      where: { id: args.id },
+    }, info);
+  },
 }
 
 module.exports = Mutations
