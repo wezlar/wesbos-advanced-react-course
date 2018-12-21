c110c79bb16896dd76d2a8172f3567c4f5ea2f68
Dean Pugh <dean.pugh@15gifts.com>
2018-12-21 16:18:40 +0000

    Video 51 -

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -269,7 +269,8 @@ const Mutations = {
             price 
             id 
             description 
-            image 
+            image
+            largeImage
           } 
         } 
       }`
@@ -287,9 +288,33 @@ const Mutations = {
       source: args.token,
     });
     // 4. convert the cart items to order items
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem = {
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } },
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
     // 5. create the order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } },
+      }
+    });
     // 6. clean up - clear the users cart, delete cart items
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({ 
+      where: { 
+        id_in: cartItemIds 
+      }, 
+    });
     // 7. return the order to the client
+    return order;
   }
 }
 
--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -28,17 +28,18 @@ function totalItems(cart) {
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = (res, createOrder) => {
+  onToken = async (res, createOrder) => {
     console.log('On token called');
     console.log(res);
     // manually call the mutation once we have the stripe token
-    createOrder({
+    const order = await createOrder({
       variables: {
         token: res.id
       }
     }).catch(err => {
       alert(err.message);
     });
+    console.log(order);
   };
   render() {
     return (
@@ -50,7 +51,7 @@ class TakeMyMoney extends React.Component {
                 amount={calcTotalPrice(me.cart)}
                 name="Sick Fits"
                 description={`Order of ${totalItems(me.cart)} items`}
-                image={me.cart[0].item && me.cart[0].item.image}
+                image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
                 stripeKey="pk_test_hBXIL11ofKlTY7o3caSf83dM"
                 currency="GBP"
                 email={me.email}
