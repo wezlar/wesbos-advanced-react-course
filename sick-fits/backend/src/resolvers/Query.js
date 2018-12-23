6dc262e1dffee543b03fb453e8d569a201f98713
Dean Pugh <dean.pugh@15gifts.com>
2018-12-23 21:49:48 +0000

    Video 53 - orders display page

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -46,6 +46,17 @@ const Query = {
         }
         // 4. return the order
         return order;
+    },
+    async orders(parent, args, ctx, info) {
+        const { userId } = ctx.request;
+        if (!userId) {
+            throw new Error('You must be signed in!');
+        }
+        return ctx.db.query.orders({
+            where: {
+                user: { id: userId }
+            }
+        }, info);
     }
 };
 
